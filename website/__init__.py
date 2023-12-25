25c23e9887c49aa23113edcf36014bf922710d2b
jan <stawa29@interia.pl>
2023-12-25 23:48:49 +0100

    form validation, preparing sql

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,19 @@
 from common import JsonReader
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 secrets_file_path = "./keys/secrets.json"
 keys_reader = JsonReader(secrets_file_path)
 keys = keys_reader.read_json()
 
+db = SQLAlchemy()
+DB_NAME = 'database.db'
 def create_app():
 
 
     app = Flask(__name__)
     app.config['SECRET_KEY'] = keys['flask_secret']
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
     from .views import views
     from .auth import auth
--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,34 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
+
     return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
     return render_template('logout.html')
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email)<4:
+            flash('Email too short', category='error')
+        elif len(name)<3:
+            flash('Name must be longer than 2 characters', category='error')
+        elif len(password1) <6:
+            flash('Password must be at least 6 characters long', category='error')
+        elif password1 != password2:
+            flash('Passwords dont match', category='error')
+        else:
+            flash('Account created', category='success')
+
+
     return render_template('sign-up.html')
