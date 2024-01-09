e0fdd308b83340c823a3f5601dff318b69b1b302
jan <stawa29@interia.pl>
2024-01-09 22:56:53 +0100

    user creation handled

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+
+from . import db
+from .models import User
 
 auth = Blueprint('auth', __name__)
 
@@ -28,7 +33,11 @@ def sign_up():
         elif password1 != password2:
             flash('Passwords dont match', category='error')
         else:
+            new_user = User(email=email,password=generate_password_hash(password1, method="sha256"),first_name=name)
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created', category='success')
+            return redirect(url_for('views.home'))
 
 
     return render_template('sign-up.html')
