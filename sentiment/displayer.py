99aca368ca02fe556ec7f1c6e0aae4d0b46be600
jan <stawa29@interia.pl>
2024-01-18 22:35:14 +0100

    adjusted colors for pie chart and timeline

--- sentiment/displayer.py
+++ sentiment/displayer.py
@@ -7,18 +7,30 @@ class Displayer:
     def __init__(self,dataframe):
         self.dataframe = dataframe
 
+    custom_colors = {
+        'Negative': 'orange',
+        'Neutral': 'gray',
+        'Positive': 'lightgreen',
+        'Strongly Negative': 'red',
+        'Strongly Positive': 'green'
+    }
+
     def display_pie(self):
+
         # Create an interactive pie chart with text labels
-        result = self.dataframe.groupby('sentiment_results_adjusted')['text'].apply(lambda x: '<br>'.join(x)).reset_index().rename(columns={'text': 'concat_text'})
+        result = self.dataframe.groupby('sentiment_results_adjusted')['text'].apply(
+            lambda x: '<br>'.join(x)).reset_index().rename(columns={'text': 'concat_text'})
         self.dataframe = self.dataframe.merge(result, on='sentiment_results_adjusted', how='left')
-        fig = px.pie(self.dataframe, names='sentiment_results_adjusted',title='Sentiment analysis',
-                     hover_name='sentiment_results_adjusted', hover_data={"concat_text": True})
+
+        fig = px.pie(self.dataframe,names='sentiment_results_adjusted',title='Sentiment analysis',
+                     hover_name='sentiment_results_adjusted', hover_data="concat_text",color='sentiment_results_adjusted',color_discrete_map=Displayer.custom_colors)
 
         # Set how text labels are displayed on the chart
         fig.update_traces(textposition='inside', textinfo='percent+label')
 
         # Show the interactive chart
         return fig.to_html(full_html=False)
+
 
 
 
@@ -33,18 +45,10 @@ class Displayer:
         grouped_df_pivot = grouped_df.pivot(index='date', columns='sentiment_results_adjusted', values='count').fillna(
             0).reset_index()
 
-        custom_colors = {
-            'Negative': 'orange',
-            'Neutral': 'gray',
-            'Positive': 'lightgreen',
-            'Strongly Negative': 'red',
-            'Strongly Positive': 'green'
-        }
-
         fig = px.line(grouped_df_pivot, x='date', y=grouped_df_pivot.columns[1:],
                       title='Sentiment Analysis Timeline by Day',
                       labels={'date': 'Date', 'value': 'Count', 'variable': 'Sentiment'},
-                      color_discrete_map=custom_colors)
+                      color_discrete_map=Displayer.custom_colors)
 
         return fig.to_html(full_html=False)
 
